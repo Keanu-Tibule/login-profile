4ab9c13e7ad3c7c5e90f6aff6b62ef6e6d49a126
Keanu Tibule <keanudane@yahoo.com>
2024-03-21 16:56:44 +0800

    Home php and css edit

--- home.php
+++ home.php
@@ -9,9 +9,25 @@ if(!isset($_SESSION["username"])){ //checks if the user is logged. isset checks 
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Home</title>
+        <link rel="stylesheet" href="home.css">
     </head>
     <body>
-        <h1>Hello World</h1>
+        <header>
+            <div class="logo">
+                <img src="media/KDLogoBlack.png" alt="Logo">
+            </div>
+            <nav>
+                <ul class="menu">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+        <h1>Hello<?php echo $_SESSION["username"];?></h1>
     </body>
 </html>
--- index.php
+++ index.php
@@ -8,9 +8,12 @@
     <body>
         <div class="container">
             <h1>LOGIN</h1>
-            <?php if (isset($_GET["error"]) && $_GET["error"] == 1): ?>
+            
+            <!--checks if there is an error displayed at the URL query AND if the error is = 1-->
+            <?php if (isset($_GET["error"]) && $_GET["error"] == 1): ?> 
                 <p style="color: red;">Invalid username or password.</p>
             <?php endif; ?>
+
             <form method="post" action="login.php">
                 <label for="username">Username: </label>
                 <input type="text" id="username" name="username" required>
